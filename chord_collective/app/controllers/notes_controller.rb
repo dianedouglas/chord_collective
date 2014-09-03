ef8cca460c928f3d85b70495553091e3873eeb8b
Diane Douglas <diane.douglas1.com>
2014-09-03 10:58:42 -0700

    Add edit and update actions and views for notes.

--- chord_collective/app/controllers/notes_controller.rb
+++ chord_collective/app/controllers/notes_controller.rb
@@ -16,20 +16,27 @@ class NotesController < ApplicationController
       flash[:notice] = "Successfully created #{@note.name}!"
       redirect_to('/notes')
     else
-  		render('notes/new.html.erb')
+      render('notes/new.html.erb')
     end
   end
 
   def show
-
+    @note = Note.find(params[:id])
+    render('notes/show.html.erb')
   end
 
   def edit
-
+    @note = Note.find(params[:id])
+    render('notes/edit.html.erb')
   end
 
   def update
-
+    @note = Note.find(params[:id])
+    if @note.update(params[:note])
+      redirect_to('/notes')
+    else
+      render('notes/edit.html.erb')
+    end
   end
 
   def destroy
