ab7c3d7193a6a1abf51c94cd6e1f5b7ef8ad3e57
Diane Douglas <diane.douglas1@gmail.com>
2014-09-04 16:01:39 -0700

    Add validation for new note parameters.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,10 @@
 class Note < ActiveRecord::Base
 
-	validates :name, :presence => true
+  validates :name, :presence => true
+  validates :octave, :presence => true
+  validates :identifier, :presence => true
+  validates :instrument, :presence => true
+	validates :loudness, :presence => true
 	has_and_belongs_to_many :chords
 
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 describe Note do
-	
-	it { should validate_presence_of :name }
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :octave }
+  it { should validate_presence_of :identifier }
+  it { should validate_presence_of :instrument }
+	it { should validate_presence_of :loudness }
 	it { should have_and_belong_to_many :chords }
-	
+
 end
