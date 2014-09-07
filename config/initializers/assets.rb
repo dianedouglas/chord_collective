489ddb8a2cb6e8dd8e3c6dd1e867a82508bf445f
dianedouglas <diane.douglas1@gmail.com>
2014-09-06 23:34:39 -0700

    Add custom stylesheet to app.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( styles.css )
+# stylesheet_link_tag "styles.css"
